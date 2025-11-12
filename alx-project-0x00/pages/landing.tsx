bc05ffb9cb194b6b536eea91212d4e0137211d50
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-11-12 10:27:22 +0100

    Creating a button component that accepts title and styles as an argument.

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,23 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Card />
+      <Card />
+      <Card />
+
+      <div className="space-x-4 mt-4">
+        {/* Small Button */}
+        <Button title="Small Button" size="small" shape="rounded-sm" />
+        {/* Medium Button */}
+        <Button title="Medium Button" size="medium" shape="rounded-md" />
+        {/* Large Button */}
+        <Button title="Large Button" size="large" shape="rounded-full" />
+      </div>
     </div>
   );
 };
